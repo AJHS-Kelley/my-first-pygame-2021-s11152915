26de800595d40c20c2c4428107079d42e365416d
s11152915 <70713796+s11152915@users.noreply.github.com>
2021-11-30 14:48:44 -0500

    v0.5
    Choose the background color

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# My First Pygame, Jermaine Jeffcoat, 11/30/21, 2:13pm, v0.4
+# My First Pygame, Jermaine Jeffcoat, 11/30/21, 2:13pm, v0.5
 
 import pygame, sys
 from pygame.locals import *
@@ -26,3 +26,7 @@ text = basicFont.render('Hello, world!', True, WHITE, BLUE)
 textRect = text.get_rect()
 textRect.centerx = windowSurface.get_rect().centerx
 textRect.centery = windowSurface.get_rect().centery
+
+# Draw the game background.
+windowSurface.fill(SHELLGREEN)
+
